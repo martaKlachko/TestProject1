ba2b38cf88d215925deeef1749b78dc1b2822729
Marta_Kurman <marta.klachko@gmail.com>
2018-11-09 12:57:59 +0200

    master3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
         System.out.println("master.1");
         System.out.println("master.2");
+        System.out.println("master.3");
+
         System.out.println("dev2.1");
         System.out.println("dev2.2");
 
